6fa37179a627e1f465ab3658881a4d7315e4aecf
adriatic <nik@congral.us>
2026-05-16 16:52:39 -0700

    fix: hardcode production Clerk publishable key in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_live_Y2xlcmsuc3JvYS5zaXRlJA",
+  },
 };
 
 export default nextConfig;
